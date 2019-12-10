df2eee8d7104a728124efeeb12449be0671b59d7
wanghaichao <m18888106873@163.com>
2019-12-11 04:00:21 +0800

    尝试无头浏览器

--- src/main/java/demo/spring/boot/demospringboot/service/WebDriverService.java
+++ src/main/java/demo/spring/boot/demospringboot/service/WebDriverService.java
@@ -1,12 +1,16 @@
 package demo.spring.boot.demospringboot.service;
 
+import demo.spring.boot.demospringboot.controller.generate.LinkedinController;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -16,14 +20,18 @@ import java.util.List;
 public class WebDriverService {
 
 
+    private static Logger LOGGER = LoggerFactory.getLogger(WebDriverService.class);
+
     public static void test() {
         try {
             File file = ResourceUtils.getFile("classpath:chromedriver");
             // 设置系统属性
             System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
+            ChromeOptions options = new ChromeOptions();
+//            options.addArguments("--headless");
             // 实例化ChromeDriver
-            WebDriver driver = new ChromeDriver();
+            WebDriver driver = new ChromeDriver(options);
+
 
             // 启动浏览器 打开百度
             driver.navigate().to("https://www.linkedin.com/login");
@@ -55,6 +63,9 @@ public class WebDriverService {
             Thread.sleep(1000);
             //点击 确定
             chromeDriver.findElement(By.xpath("//span[text()='确定']")).click();
+            Thread.sleep(1000);
+            String text = chromeDriver.findElement(By.xpath("//div[@class='display-flex']")).getText();
+            LOGGER.info("text:{}", text);
 
 
         } catch (FileNotFoundException | InterruptedException e) {
