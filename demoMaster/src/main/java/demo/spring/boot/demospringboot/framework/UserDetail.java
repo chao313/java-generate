c09eb692b0fadfc1d43ae314b7b75d405d8ed101
wanghaichao <m18888106873@163.com>
2020-05-13 00:29:06 +0800

    补全丢失的UserDetail.java

---
+++ demoMaster/src/main/java/demo/spring/boot/demospringboot/framework/UserDetail.java
@@ -0,0 +1,28 @@
+package demo.spring.boot.demospringboot.framework;
+
+public interface UserDetail {
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * @return the password
+     */
+    String getPassword();
+
+    /**
+     * Returns the username used to authenticate the user. Cannot return <code>null</code>
+     * .
+     *
+     * @return the username (never <code>null</code>)
+     */
+    String getUsername();
+
+
+    /**
+     * 获取用户id
+     *
+     * @return the id (never <code>null</code>)
+     */
+    String getId();
+
+
+}
