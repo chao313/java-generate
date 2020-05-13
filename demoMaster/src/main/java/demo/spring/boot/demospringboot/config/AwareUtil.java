12caef84a9ec42d187aba4170ad6800b572f915b
wanghaichao <m18888106873@163.com>
2020-05-13 18:41:16 +0800

    修复包 bug

--- demoMaster/src/main/java/demo/spring/boot/demospringboot/config/AwareUtil.java
+++ demoMaster/src/main/java/demo/spring/boot/demospringboot/config/AwareUtil.java
@@ -1,4 +1,4 @@
-package demo.kafka.config;
+package demo.spring.boot.demospringboot.config;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
